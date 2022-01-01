a69ad69bfc1e57ddc254167f7c2d5ab56126bbb9
mohmed30 <48033127+mohmed30@users.noreply.github.com>
2022-01-01 20:02:31 +0200

    Update cal.asm

--- cal.asm
+++ cal.asm
@@ -7,3 +7,21 @@
 	include C:\irvine\Irvine32.inc
   
   .date
+                Zero = 0
+		MAX = 100
+		stringIn BYTE MAX+1 DUP(?), 0
+		string1 BYTE MAX+1 DUP(?), 0
+		string2 BYTE MAX+1 DUP(?), 0
+		SYM_ADD BYTE '+'
+		SYM_SUB BYTE '-'
+		SYM_MUL BYTE '*'
+		SYM_DIV BYTE '/'
+		CURR_SYM_ADD BYTE '+'
+		CURR_SYM_MUL BYTE '*'
+		RES_ADD DWORD 0
+		RES_MUL DWORD 1
+		PARSE_RES DWORD 0
+		errorChick DWORD 1
+		errorMag  db 'Math Error can not divide by zero please Enter the right equation',0
+		
+.code
